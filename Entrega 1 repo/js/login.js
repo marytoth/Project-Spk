5eab578fc3ddd5f861fb7726c9f8d9a56a0d2110
marytoth <68874630+marytoth@users.noreply.github.com>
2020-08-15 21:13:02 -0300

    Add files via upload

---
+++ Entrega 1 repo/js/login.js
@@ -0,0 +1,14 @@
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function(e){
+
+});
+
+function checklogin(user, pass) {
+	if(user.trim() === "" || pass.trim() === "") {
+		alert("Complete los datos, por favor.");
+	} else {
+		location.href = "login.html";
+	}
+}
