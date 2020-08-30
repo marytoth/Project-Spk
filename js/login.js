78169852e6ec387999a2388d28a422e28b57f7e0
marytoth <68874630+marytoth@users.noreply.github.com>
2020-08-30 12:56:36 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,9 +1,7 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
-});
 
 function checklogin(user, pass) {
 	if(user.trim() === "" || pass.trim() === "") {
@@ -14,3 +12,7 @@ function checklogin(user, pass) {
 		location.href = "login.html";
 	}
 }
+
+document.addEventListener("DOMContentLoaded", function(e){
+
+});
