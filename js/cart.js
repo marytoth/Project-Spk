ce7973426a9e8aa8e5090f4af95f6cd757e794ee
gloriamendezmendez@gmail.com <gloriamendezmendez@gmail.com>
2020-10-10 21:10:49 -0300

    Entrega 5- cart

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,8 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
- 
+
+
 var contenido= JSON.parse(localStorage.getItem("lista"));
     var lista="";
     var cantidad=0;
@@ -18,56 +19,28 @@ var contenido= JSON.parse(localStorage.getItem("lista"));
        document.getElementById('cartCont').innerHTML=cantidad;
        //document.getElementById('aremove').innerHTML=remove; 
 
+ document.addEventListener("DOMContentLoaded", function(e){
+
+})
+
+       getJSONData(CART_INFO_URL).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      let cart = resultObj.data.articles;
+      
+      let textToAppend = ``
+      for (let a = 0; a < cart.length; a++) {
+        textToAppend += `
+        <tr>
+        <td><img src=''` + cart[a].src + `">
+        </td>
+        <td></td>
+        </tr>
+        `
+
+      }
+ 
+      document.getElementById('cart').innerHTML= textToAppend;
 
 
-var lista=[];
-        function guardar(){
-            var personas={}; //defino el objeto
-            var nom=document.getElementById('nombre').value;//obtengo los valores
-            var edad=document.getElementById('edad').value;
 
-            personas.nombre= nom;//guardo los valores
-            personas.edad=edad;
-
-            
-            lista.push(personas); //agrego los valores al final de la colección
-            localStorage.setItem("lista",JSON.stringify(lista)); //Guardo la lista en localStorage
-            mostrar(lista);//llamo a la funcion para mostrar los datos
-        }
-
-        function mostrar(lista){
-
-        var tabla = "<table border =1> <th> Productos </th><th> Precios </th>"; //escribe el encabezado
-        for (i=0; i<lista.length; i++){
-
-            tabla+="<tr align='center'><td>" + lista[i].nombre + "</td><td>" + lista[i].edad +"</td></tr>";
-        }
-        tabla+="</table>";
-        document.getElementById('lista').innerHTML=tabla;
-        }
-
-        function ordenar(){
-            lista.sort( function(ant, sig){
-                return ant.edad-sig.edad; //comparo el campo edad
-            });
-            mostrar(lista);
-        }
-        function ordenonombre(){
-            lista.sort( function(ant, sig){ //comparando el campo nombre
-                if (ant.nombre > sig.nombre){
-                    return 1;
-                }
-                if (ant.nombre < sig.nombre) {
-                    return -1;
-                } 
-                return 0;
-            
-            });
-            mostrar(lista);
-        }
-  
-        
-
-document.addEventListener("DOMContentLoaded", function(e){
-
-});
+    }})
