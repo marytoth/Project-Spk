52747f88c0b4d902345b08d697f8eac0f39a45f3
gloriamendezmendez@gmail.com <gloriamendezmendez@gmail.com>
2020-10-11 13:56:28 -0300

    Entrega 5. cart2

--- js/cart.js
+++ js/cart.js
@@ -2,45 +2,41 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 
+var cart=[]; //array para guardar la colección de artículos.
 
-var contenido= JSON.parse(localStorage.getItem("lista"));
-    var lista="";
-    var cantidad=0;
-    if (contenido!=null && contenido.length>0){
-       cantidad=contenido.length;
-       contenido.lista.forEach(element => {
-         lista+= "<li>" + product.name + "-- $ " + product.cost + "</li>";         
-       });
-       } else {
-            lista+="<div class='alert alert-warning'> Tu carrito esta vacío </div>";
-       }
+function mostrar(cart){
+  
+    var dataTable="";
+    for (i=0; i<cart.length;i++){
+      let article = cart[i];
+      let subTotal = parseInt(article.unitCost)*parseInt(article.count);
 
-       document.getElementById('cart').innerHTML=lista;
-       document.getElementById('cartCont').innerHTML=cantidad;
-       //document.getElementById('aremove').innerHTML=remove; 
+      dataTable+=`<tr><td><img src="${article.src}" width=60></td>`;
+      dataTable+=`<td><input type="number" id="cant${i}" value=${article.count} > </td>`;
+      dataTable+=`<td>${article.name}</td>`;
+      dataTable+=`<td>${article.unitCost} + ${article.currency} </td>`;
+      dataTable+=`<td>${subTotal}</td>`;
+      dataTable+=`</tr>`;
+    }
+    document.getElementById('carrito').innerHTML=dataTable;
+     document.getElementbyId("subtotal").innerHTML= subTotal;
+    }
 
- document.addEventListener("DOMContentLoaded", function(e){
+     
 
-})
+     //document.getElementbyId("subtotal").innerHTML= subTotal;
+  
+  
+     document.addEventListener("DOMContentLoaded", function(e){
 
        getJSONData(CART_INFO_URL).then(function (resultObj) {
-    if (resultObj.status === "ok") {
-      let cart = resultObj.data.articles;
-      
-      let textToAppend = ``
-      for (let a = 0; a < cart.length; a++) {
-        textToAppend += `
-        <tr>
-        <td><img src=''` + cart[a].src + `">
-        </td>
-        <td></td>
-        </tr>
-        `
 
-      }
- 
-      document.getElementById('cart').innerHTML= textToAppend;
+        if (resultObj.status === "ok") {
+           var cart = resultObj.data.articles;//guardo la colección de artículos en cart
+            //es un array
+            //alert (`Obtuve ${cart.length} elementos`);
+        }
 
-
-
-    }})
+        
+          mostrar(cart);
+         })})
